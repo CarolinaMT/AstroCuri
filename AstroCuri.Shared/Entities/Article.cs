f5b34aa6c619f7ce52f57d30aafdfaa576772d2a
SOFIA CASTRILLON BUILES <sofiacastrillon287773@correo.itm.edu.co>
2023-10-22 15:44:00 -0500

    .

---
+++ AstroCuri.Shared/Entities/Article.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AstroCuri.Shared.Entities
+{
+    public class Article
+    {
+        [Display(Name = "Id")]
+        [MaxLength(20, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Id { get; set; } = null!;
+
+        [Display(Name = "Id")]
+        public int UserId { get; set; }
+
+        [Display(Name = "Título")]
+        [MaxLength(100, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Titulo { get; set; }
+
+        [Display(Name = "Contenido")]
+        [MaxLength(1000, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Contenido { get; set; }
+
+        [Display(Name = "Fecha de Publicación")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public DateTime Fecha_publi { get; set; }
+
+        [Display(Name = "Categoría")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public int CategoriaID { get; set; }
+    }
+}
